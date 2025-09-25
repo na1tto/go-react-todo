6c9e8c97dd2e6c046cfb8e5224d745235a8e53e0
na1tto <eduardo3245.ss@gmail.com>
2025-09-25 18:38:16 -0300

    feat: add .env e .gitignore, conclui os endpoints DELETE e GET

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -18,11 +21,18 @@ func main() {
 	fmt.Println("Hello World!")
 	app := fiber.New()
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error load .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
 	todos := []Todo{}
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
 
-		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+		return c.Status(200).JSON(todos)
 	})
 
 	// insere uma todo na lista (endpoint)
@@ -32,10 +42,10 @@ func main() {
 		if err := c.BodyParser(todo); err != nil {
 			return err
 		}
-		
+
 		// se o corpo da request for vazio
 		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error":"Todo body is required"})
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
 		}
 
 		// atribui o id da todo e insere na lista de todos
@@ -61,5 +71,19 @@ func main() {
 		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	// Deleta uma TODO
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"success": true})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	log.Fatal(app.Listen(":" + PORT))
 }
